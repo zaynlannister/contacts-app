784e31dc08d54bbbe2cda17bea37cb96e356ac9c
zaynlannister <zaynlannister@gmail.com>
2023-10-17 12:08:56 +0500

    feat: create react-app & install necessary packages

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet,
+          pariatur.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
